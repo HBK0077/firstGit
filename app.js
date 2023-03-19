e31f158f13fea1721dc27221bd4f96dbbdef458c
HBK0077 <hrishikeshbalakrishna7@gmail.com>
2023-03-19 13:04:12 +0530

    2nd eement green and 3rd element invisble

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-// 
-console.log(document.getElementsByTagName("li"));
-
-console.log(document.getElementsByClassName("list-group-item"));
+var lis = document.querySelectorAll("li");
+//console.log(lis);
+for(var i=0; i<lis.length; i++){
+    if(i==1){
+        lis[i].style.backgroundColor = "green";
+    }
+    if(i==2){
+        lis[i].style.display = "none";
+    }
+}
