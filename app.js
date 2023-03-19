8fd6161e5d7723d588d9150e14dca1319d26b5fc
HBK0077 <hrishikeshbalakrishna7@gmail.com>
2023-03-19 15:35:25 +0530

    Creating nodes and modifying DOMS

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-var lis = document.querySelectorAll("li");
-//console.log(lis);
-for(var i=0; i<lis.length; i++){
-    if(i==1){
-        lis[i].style.backgroundColor = "green";
-    }
-    if(i==2){
-        lis[i].style.display = "none";
-    }
-}
+var txt = document.querySelector("#items");
+//console.log(txt);
+//console.log(txt.parentNode);
+txt.parentNode.textContent = "Hello"; 
+
