b38600efd05fb392911d3b3a037c79d57f80154c
HBK0077 <hrishikeshbalakrishna7@gmail.com>
2023-03-20 10:39:47 +0530

    button adding and deletion completed

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
-var txt = document.querySelector("#items");
-//console.log(txt);
-//console.log(txt.parentNode);
-txt.parentNode.textContent = "Hello"; 
+var form = document.getElementById("addForm");
 
+var itemList = document.getElementById("items");
+
+//form submit event
+form.addEventListener("submit", addItem);
+
+//delete event
+itemList.addEventListener("click", removeItem);
+
+//add item function
+
+function addItem(e){
+    e.preventDefault();
+    //getinput value
+    var newItem = document.getElementById("item").value;
+
+    // create new li element
+
+    var li = document.createElement("li");
+    //add class
+    li.className = "list-group-item";
+    //add text node with input node
+    li.appendChild(document.createTextNode(newItem));
+
+    //create delete button element
+
+    var deleteButton = document.createElement("button");
+    //add classes to delete button
+    deleteButton.className = "btn btn-danger btn-sm float-right delete";
+    //append the text node
+    deleteButton.appendChild(document.createTextNode("X"));
+    
+    //append button to li
+    li.appendChild(deleteButton);
+
+    itemList.appendChild(li);
+}
+
+function removeItem(e){
+    if(e.target.classList.contains("delete")){
+        if(confirm("are you sure?")){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
